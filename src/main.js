f80032b5648ab44f5985c97ad6fd7c08e08f292d
JustinPansacola <pansacolajustin@gmail.com>
2023-07-18 16:00:19 +0800

    Changes

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '../node_modules/@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '../node_modules/@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faUserSecret } from '../node_modules/@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '../node_modules/@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
 library.add(faUserSecret,faGithub, faLinkedin)
