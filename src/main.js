786b2f7a91f0298177710ee76ca573573988126b
JustinPansacola <pansacolajustin@gmail.com>
2023-07-18 16:14:30 +0800

    Last Try

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from '../node_modules/vue/dist/vue.esm-bundler.js'
+import { createApp } from './../node_modules/vue/dist/vue.esm-bundler.js'
 import './style.css'
 import App from './App.vue'
 
-import { library } from '../node_modules/@fortawesome/fontawesome-svg-core'
+import { library } from './../node_modules/@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '../node_modules/@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from './../node_modules/@fortawesome/vue-fontawesome'
+
 
 /* import specific icons */
-import { faUserSecret } from '../node_modules/@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '../node_modules/@fortawesome/free-brands-svg-icons'
+import { faUserSecret } from './../node_modules/@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from './../node_modules/@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
 library.add(faUserSecret,faGithub, faLinkedin)
