bc7c196af766768809467ec8e717a8f83ec29c35
JustinPansacola <pansacolajustin@gmail.com>
2023-07-18 17:09:52 +0800

    Removes FA

--- src/main.js
+++ src/main.js
@@ -2,19 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+// import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+// import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret,faGithub, faLinkedin)
+// library.add(faUserSecret,faGithub, faLinkedin)
 
 
 createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon)
+// .component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
